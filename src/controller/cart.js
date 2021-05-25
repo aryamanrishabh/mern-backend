efe43dee5fd552570592a68852dd781753c98fe1
aryamanrishabh <aryamanrishabh@gmail.com>
2021-05-25 13:49:18 +0530

    add item to cart

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -2,17 +2,25 @@ const Cart = require('../models/cart');
 
 exports.addItemToCart = (req, res) => {
     
-    //Cart.find({ user: req.user._id})
-    
-    const cart = new Cart({
-        user: req.user._id,
-        cartItems: req.body.cartItems
-    });
-
-    cart.save((error, cart) => {
+    Cart.findOne({ user: req.user._id}).exec((error, cart) => {
         if(error) return res.status(400).json({error});
         if(cart){
-            res.status(201).json({ cart });
+
+            res.status(200).json({message: cart});
+
+        }
+        else {
+            const cart = new Cart({
+                user: req.user._id,
+                cartItems: req.body.cartItems
+            });
+        
+            cart.save((error, cart) => {
+                if(error) return res.status(400).json({error});
+                if(cart){
+                    res.status(201).json({ cart });
+                }
+            });
         }
     });
 
